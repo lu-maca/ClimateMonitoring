28fb4000deb17e581956317928437d5338d028d0
lucamacavero <luca.macavero@gmail.com>
2023-04-02 23:40:55 +0200

    graphics initial commit

---
+++ src/main/java/uni/climatemonitor/graphics/MainWindow.java
@@ -0,0 +1,11 @@
+package uni.climatemonitor.graphics;
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    JFrame w = new JFrame();
+    public MainWindow() {
+        w.setSize(400,500);
+        w.setLayout(null);
+        w.setVisible(true);
+    }
+}
