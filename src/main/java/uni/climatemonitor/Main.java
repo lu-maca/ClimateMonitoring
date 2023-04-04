ba117ad93179012476dc4318889d4272378d7089
lucamacavero <luca.macavero@gmail.com>
2023-04-05 00:11:09 +0200

    wip: add buttons

--- src/main/java/uni/climatemonitor/Main.java
+++ src/main/java/uni/climatemonitor/Main.java
@@ -3,7 +3,14 @@ package uni.climatemonitor;
 import uni.climatemonitor.graphics.MainWindow;
 
 public class Main {
-    public static void main(String[] args) {
-            MainWindow app = new MainWindow();
-        }
+    private static MainWindow frame;
+    private static Main program;
+
+    public Main() {
+        frame = new MainWindow();
     }
+
+    public static void main(String[] args) {
+        program = new Main();
+    }
+}
--- src/main/java/uni/climatemonitor/graphics/MainWindow.java
+++ src/main/java/uni/climatemonitor/graphics/MainWindow.java
@@ -1,11 +1,34 @@
 package uni.climatemonitor.graphics;
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
-    JFrame w = new JFrame();
+    private static SearchBar searchBut;
+    private static JButton loginBut;
+
     public MainWindow() {
-        w.setSize(400,500);
-        w.setLayout(null);
-        w.setVisible(true);
+        /* Set main window properties */
+        super("Climate Monitor");
+        setSize(600,600);
+        setLocationRelativeTo(null);
+        setLayout(new GridBagLayout());
+        GridBagConstraints grid = new GridBagConstraints();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /* add a search bar to the main window */
+        searchBut = new SearchBar();
+        searchBut.setVisible(true);
+        add(searchBut);
+
+        /* add a login/register button to the main window */
+        loginBut = new JButton("Login/register");
+        loginBut.setVisible(true);
+        grid.anchor = GridBagConstraints.NORTHEAST;
+        add(loginBut, grid);
+
+
+        /* set main window visibility */
+        setVisible(true);
     }
+
 }
