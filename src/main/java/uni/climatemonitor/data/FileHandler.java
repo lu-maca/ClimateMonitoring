e0702f587558908f0cfe8e8496d2f33f5215b5e5
Luca <luca.macavero@dorbit.space>
2023-04-09 23:43:27 +0200

    update filehandler class

--- src/main/java/uni/climatemonitor/data/FileHandler.java
+++ src/main/java/uni/climatemonitor/data/FileHandler.java
@@ -1,4 +1,17 @@
 package uni.climatemonitor.data;
 
-public class FileHandler {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public abstract class FileHandler {
+    private final String fileName;
+    private String fileContent = null;
+
+    public FileHandler(String fileName){
+        this.fileName = fileName;
+    }
+
+
 }
